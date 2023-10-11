a2d9a88cff65be3bef5a9868396b3325944b0cf2
staticf0x <staticf0x@pm.me>
2023-10-11 10:56:16 +0200

    Add tests

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,6 +1,10 @@
+import pytest
+
 from docstring_to_markdown.google import google_to_markdown, looks_like_google
 
 BASIC_EXAMPLE = """Do **something**.
+
+Some more detailed description.
 
 Args:
     a: some arg
@@ -12,19 +16,104 @@ Returns:
 
 BASIC_EXAMPLE_MD = """Do **something**.
 
-# Args
+Some more detailed description.
 
-- a: some arg
-- b: some arg
+#### Args
 
-# Returns
+- `a`: some arg
+- `b`: some arg
+
+#### Returns
 
 - Same *stuff*
 """
 
+ESCAPE_MAGIC_METHOD = """Example.
 
-def test_looks_like_google_recognises_google():
-    assert looks_like_google(BASIC_EXAMPLE)
+Args:
+    a: see __init__.py
+"""
+
+ESCAPE_MAGIC_METHOD_MD = """Example.
+
+#### Args
+
+- `a`: see \\_\\_init\\_\\_.py
+"""
+
+PLAIN_SECTION = """Example.
+
+Args:
+    a: some arg
+
+Note:
+    Do not use this.
+
+Example:
+    Do it like this.
+"""
+
+PLAIN_SECTION_MD = """Example.
+
+#### Args
+
+- `a`: some arg
+
+#### Note
+
+Do not use this.
+
+#### Example
+
+Do it like this.
+"""
+
+MULTILINE_ARG_DESCRIPTION = """Example.
+
+Args:
+    a (str): This is a long description
+             spanning over several lines
+        also with broken indentation
+    b (str): Second arg
+"""
+
+MULTILINE_ARG_DESCRIPTION_MD = """Example.
+
+#### Args
+
+- `a (str)`: This is a long description
+             spanning over several lines
+             also with broken indentation
+- `b (str)`: Second arg
+"""
+
+GOOGLE_CASES = {
+    "basic example": {
+        "google": BASIC_EXAMPLE,
+        "md": BASIC_EXAMPLE_MD,
+    },
+    "escape magic method": {
+        "google": ESCAPE_MAGIC_METHOD,
+        "md": ESCAPE_MAGIC_METHOD_MD,
+    },
+    "plain section": {
+        "google": PLAIN_SECTION,
+        "md": PLAIN_SECTION_MD,
+    },
+    "multiline arg description": {
+        "google": MULTILINE_ARG_DESCRIPTION,
+        "md": MULTILINE_ARG_DESCRIPTION_MD,
+    },
+}
+
+
+@pytest.mark.parametrize(
+    "google",
+    [case["google"] for case in GOOGLE_CASES.values()],
+    ids=GOOGLE_CASES.keys(),
+)
+def test_looks_like_google_recognises_google(google):
+    assert looks_like_google(google)
 
 
 def test_looks_like_google_ignores_plain_text():
@@ -32,5 +121,10 @@ def test_looks_like_google_ignores_plain_text():
     assert not looks_like_google("See Also\n--------\n")
 
 
-def test_google_to_markdown():
-    assert google_to_markdown(BASIC_EXAMPLE) == BASIC_EXAMPLE_MD
+@pytest.mark.parametrize(
+    "google,markdown",
+    [[case["google"], case["md"]] for case in GOOGLE_CASES.values()],
+    ids=GOOGLE_CASES.keys(),
+)
+def test_google_to_markdown(google, markdown):
+    assert google_to_markdown(google) == markdown
